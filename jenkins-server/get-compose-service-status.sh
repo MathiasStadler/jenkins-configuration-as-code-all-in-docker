92fb36ee61a34e3dd90df21a71d3502206f16464
MathiasStadler <email@mathias-stadler.de>
2019-07-17 14:22:51 +0200

    spell fix

--- jenkins-server/get-compose-service-status.sh
+++ jenkins-server/get-compose-service-status.sh
@@ -4,7 +4,7 @@ set -o errexit -o posix
 DOCKER_COMPOSE_SERVICE="$(docker-compose ps --services)";
 
 if [ "$(docker-compose ps -q |wc -l)" -eq 0 ] ; then
-printf "service %s NOT runnuning\n" "${DOCKER_COMPOSE_SERVICE}";
+printf "service %s NOT running\n" "${DOCKER_COMPOSE_SERVICE}";
 else
-printf "service %s runnuning\n" "${DOCKER_COMPOSE_SERVICE}";
+printf "service %s running\n" "${DOCKER_COMPOSE_SERVICE}";
 fi
