440fd18809d6036273e13db3efb7eece6b7dc4ac
MathiasStadler <email@mathias-stadler.de>
2019-07-17 10:58:51 +0200

    fix

--- start-jenkins.sh
+++ start-jenkins.sh
@@ -16,18 +16,27 @@ if [ -z "$COMPOSE_PROJECT_NAME" ]; then
         printf "# please export in your shell by your self\n"
         printf "export COMPOSE_PROJECT_NAME=\"%s\"\n" "$1"
     fi
+    exit 1
 else
     if [ "$#" -ne 1 ]; then
         printf "OK: \$COMPOSE_PROJECT_NAME is set to => %s \n" "$COMPOSE_PROJECT_NAME"
     else
-        printf "# ATTENTION !!! \$COMPOSE_PROJECT_NAME already set to => %s\n" "$COMPOSE_PROJECT_NAME"; 
-        printf "# Would you lke add an other project?\n" 
+        printf "# ATTENTION !!! \$COMPOSE_PROJECT_NAME already set to => %s\n" "$COMPOSE_PROJECT_NAME"
+        printf "# Would you lke add an other project?\n"
         printf "# Please set the \"COMPOSE_PROJECT_NAME by hand"
         printf "# e.g.\n"
         printf "export \$COMPOSE_PROJECT_NAME=\"%s\"\n" "$1"
-        printf "# for unset type\n";
-        printf "unset COMPOSE_PROJECT_NAME";
+        printf "# for unset type\n"
+        printf "unset COMPOSE_PROJECT_NAME"
         exit 1
     fi
 
 fi
+
+
+make build-service
+make run-service
+
+# TODO IP
+# TODO port
+echo "open jenkins on ip:port"
