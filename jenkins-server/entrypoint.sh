4dc91b766e08a9cbedd6bc68cc39b2482e454436
MathiasStadler <email@mathias-stadler.de>
2019-07-18 11:41:47 +0200

    work with start inside setup-tls-docker.sh

---
+++ jenkins-server/entrypoint.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+set -o errexit -o posix
+
+
+exec "$@"
--- jenkins-server/setup-tls-docker.sh
+++ jenkins-server/setup-tls-docker.sh
@@ -1,10 +1,12 @@
 #!/bin/bsh
 set -o errexit -o posix
 
-mkdir /var/jenkins_home/.docker
+if ! [ -d "/var/jenkins_home/.docker" ]; then
+    mkdir /var/jenkins_home/.docker
+fi
 
 ln -s /run/secrets/docker-ca.pem /var/jenkins_home/.docker/ca.pem
 ln -s /run/secrets/docker-cert.pem /var/jenkins_home/.docker/cert.pem
 ln -s /run/secrets/docker-key.pem /var/jenkins_home/.docker/key.pem
 
-exit 0;
+/sbin/tini -- /usr/local/bin/jenkins.sh
