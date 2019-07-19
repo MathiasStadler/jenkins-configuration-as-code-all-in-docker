7dd523e3903f49e3856f0bc02d377c73e6bb766d
MathiasStadler <email@mathias-stadler.de>
2019-07-19 17:25:30 +0200

    improve script

--- utilities/validate-jenkins-pipeline.sh
+++ utilities/validate-jenkins-pipeline.sh
@@ -2,15 +2,22 @@
 # from https://jenkins.io/doc/book/pipeline/development/
 set -o errexit -o posix -o pipefail -o nounset
 
+curl --version >/dev/null 2>/dev/null || { printf "curl needed, install first\n"; exit 1;}
+
+JENKINS_USER="${JENKINS_USER:-admin}";
+JENKINS_PASSWD="${JENKINS_PASSWD:-admin}";
+JENKINS_HOST_NAME="${JENKINS_HOST_NAME:-localhost}";
+JENKINS_HOST_PORT="${JENKINS_HOST_PORT:-8080}";
+JENKINS_HOST_PROTOCOL="${JENKINS_HOST_PROTOCOL:-http}";
+
 if [ "$#" -eq 1 ]; then
 
     # curl (REST API)
     # Assuming "anonymous read access" has been enabled on your Jenkins instance.
-    JENKINS_URL="http://admin:admin@localhost:8080"
+    JENKINS_URL="${JENKINS_HOST_PROTOCOL}://${JENKINS_USER}:${JENKINS_PASSWD}@${JENKINS_HOST_NAME}:${JENKINS_HOST_PORT}";
     # JENKINS_CRUMB is needed if your Jenkins master has CRSF protection enabled as it should
     JENKINS_CRUMB=$(curl "$JENKINS_URL/crumbIssuer/api/xml?xpath=concat(//crumbRequestField,\":\",//crumb)")
-    echo "JENKINS_CRUMB => ${JENKINS_CRUMB}";
-    curl -X POST -H $JENKINS_CRUMB -F "jenkinsfile=<$1" $JENKINS_URL/pipeline-model-converter/validate
+    curl -X POST -H "${JENKINS_CRUMB}" -F "jenkinsfile=<${1}" "${JENKINS_URL}"/pipeline-model-converter/validate
 
 elif [ "$#" -gt 1 ]; then
 
