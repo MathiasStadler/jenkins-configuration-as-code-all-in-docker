86c318a3f9fce2ee7c5911e8f99d5833eaaf669d
MathiasStadler <email@mathias-stadler.de>
2019-08-07 20:44:07 +0200

    chnage permission

--- jenkins-server/entrypoint.sh
+++ jenkins-server/entrypoint.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 set -o errexit -o posix -o nounset
 
-# setup tls for docker
-bash +x /setup-tls-docker.sh
 # change ${JENKINS_HOME} permission
 bash +x /jenkins-home-change-permission.sh
+# su jenkins
+su - jenkins
+# setup tls for user jenkins
+bash +x /setup-tls-docker.sh
 # start jenkins 
 # script inherit from parent images
 /sbin/tini -- /usr/local/bin/jenkins.sh
-
 # HINT ::
 # NO exec "$@" here because it is include in jenkins.sh script
