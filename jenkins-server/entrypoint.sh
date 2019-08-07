b86029b90d0637827d497581a6132ec9d14a9988
MathiasStadler <email@mathias-stadler.de>
2019-08-07 20:06:34 +0200

    add jenkins-home-change-permission.sh

--- jenkins-server/entrypoint.sh
+++ jenkins-server/entrypoint.sh
@@ -3,8 +3,11 @@ set -o errexit -o posix -o nounset
 
 # setup tls for docker
 bash +x /setup-tls-docker.sh
+# change ${JENKINS_HOME} permission
+bash +x /jenkins-home-change-permission.sh
 # start jenkins 
 # script inherit from parent images
 /sbin/tini -- /usr/local/bin/jenkins.sh
 
+# HINT ::
 # NO exec "$@" here because it is include in jenkins.sh script
---
+++ jenkins-server/jenkins-home-change-permission.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+set -o errexit -o posix -o nounset -o pipefail
+
+chown -R jenkins:jenkins  "${JENKINS_HOME:-}"
