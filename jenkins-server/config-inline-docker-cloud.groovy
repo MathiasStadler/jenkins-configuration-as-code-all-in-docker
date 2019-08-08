e4726ed8a6e261dce19d77bf9995acb63be05f7d
MathiasStadler <email@mathias-stadler.de>
2019-08-08 15:20:02 +0200

    try

--- jenkins-server/config-inline-docker-cloud.groovy
+++ jenkins-server/config-inline-docker-cloud.groovy
@@ -7,7 +7,7 @@ job('init-add-docker-cloud') {
     steps {
 
         printf "Hallo World\n"
-        println("script directory: ${new File(__FILE__).parent.absolutePath}")
+        println("script directory: ${new File('.').parent.absolutePath}")
     }
 
 
